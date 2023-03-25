7d37a7267d2c28ec491f40b7305cc89901f9a1df
sergeyshor <sss1401@inbox.ru>
2023-03-26 00:32:35 +0300

    Added main and Run functions

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+
+	"github.com/dacore-x/truckly/config"
+	"github.com/dacore-x/truckly/internal/app"
+)
+
+func main() {
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.Run(cfg)
+}
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,37 @@
 package app
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/dacore-x/truckly/config"
+	"github.com/dacore-x/truckly/pkg/pghelper"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+
+	"github.com/dacore-x/truckly/internal/infrastructure/repository/postgres"
+	v1 "github.com/dacore-x/truckly/internal/transport/http/v1"
+	"github.com/dacore-x/truckly/internal/usecase"
+)
+
+func Run(cfg *config.Config) {
+	// Repository
+	connURL := pghelper.GetConnURL(cfg.PG)
+
+	conn, err := sql.Open("postgres", connURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	// Use cases
+	userUseCase := usecase.NewUserUseCase(
+		postgres.NewUserRepo(conn),
+	)
+
+	// HTTP server
+	r := gin.Default()
+	h := v1.NewHandlers(userUseCase)
+	h.NewRouter(r)
+	r.Run()
+}
